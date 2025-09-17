b5a553bcffd4e2c684fd90640490ff5a6282743a
tabish-rahman <mdtabish.rkhan@gmail.com>
2025-09-17 12:20:51 +0530

    Fixed vpc.tf formatting

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vprofile-eks"
   cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-## data has been defined in main.tf file, we have referred from there and then used the slice function.
+  ## data has been defined in main.tf file, we have referred from there and then used the slice function.
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
